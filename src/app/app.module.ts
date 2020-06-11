eb696ffce5bef245d5e6e24d6d592f52e22a18ae
Ligiano Azzi <ldaazzi@gmail.com>
2020-06-11 23:49:48 +0100

    configuring poso-list component and route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './post-list/post-list.component';
+
+const appRoutes: Routes = [
+  {path: '', pathMatch: 'full',component: PostListComponent},
+  {path: 'posts', component: PostListComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,

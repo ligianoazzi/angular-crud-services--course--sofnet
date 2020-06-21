49c868986f10770bc466b69af2c0d52975d77bd3
Ligiano Azzi <ldaazzi@gmail.com>
2020-06-21 14:56:00 +0100

    uploading the code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,22 +4,42 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PostListComponent } from './post-list/post-list.component';
+import { RouterModule, Routes } from "@angular/router";
+import { HttpClientModule } from '@angular/common/http';
+import { PostService } from "./services/post.service";
+import { PostSaveComponent } from './post-save/post-save.component';
+import { ButtonComponent } from './bootstrap/button/button.component';
+import { FormsModule } from "@angular/forms";
+import { GlyphComponent } from './bootstrap/glyph/glyph.component';
+import { ModalComponent } from './bootstrap/modal/modal.component';
+import { AlertComponent } from './bootstrap/alert/alert.component';
+import { MessageService } from "./services/message.service";
 
 const appRoutes: Routes = [
   {path: '', pathMatch: 'full',component: PostListComponent},
-  {path: 'posts', component: PostListComponent}
+  {path: 'posts', component: PostListComponent},
+  {path: 'posts/create', component: PostSaveComponent},
+  {path: 'posts/:id/edit', component: PostSaveComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostListComponent
+    PostListComponent,
+    PostSaveComponent,
+    ButtonComponent,
+    GlyphComponent,
+    ModalComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [PostService, MessageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { PostService } from "../services/post.service";
+import {ModalComponent} from "../bootstrap/modal/modal.component";
+import {MessageService} from "../services/message.service";
+
+
 
 @Component({
   selector: 'app-post-list',
@@ -7,9 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  constructor() { }
+	posts: any = [];
+	postToDelete = null;
+	message = null;
 
-  ngOnInit() {
-  }
+	@ViewChild(ModalComponent)
+	modal: ModalComponent;
+	constructor(private postService: PostService, private messageService:MessageService) {
+		this.message = this.messageService.message;
+	}
+
+	ngOnInit() {
+		 this.postService.query().subscribe(data => this.posts = data);
+		 
+
+  	}
+
+  	/*destroy(id, index){
+  		if ( confirm('Do you would like to delete this post?') ){
+  			this.postService.destroy(+id).subscribe(() => {
+  				alert("Post deleted successfully!");
+  				this.posts.splice(index,1);
+  			})
+  		}
+  	}*/
+
+  	destroy(){
+   			this.postService.destroy(+this.postToDelete.id).subscribe(() => {
+   				let index = this.posts.indexOf(this.postToDelete);
+  				this.posts.splice(index,1);
+  				this.modal.close();
+  			}) 		
+  	}
+
+  	openModal(post){
+  		this.postToDelete = post;
+  		this.modal.open();
+
+  	}
 
 }
